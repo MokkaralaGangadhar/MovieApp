6d132c244974614e11f120681cf48dae710d7179
Vemula <vemulajohnbabu@netzary.com>
2023-11-23 22:13:54 +0530

    added comments to code

--- src/Components/MoviesList/MoviesList.jsx
+++ src/Components/MoviesList/MoviesList.jsx
@@ -8,21 +8,33 @@ import Accordion from "react-bootstrap/Accordion";
 import { useNavigate, createSearchParams, useLocation } from "react-router-dom";
 
 const MoviesList = () => {
- 
+//  For Setting MovieList
   const [searchList, setSearchList] = useState([]);
-  const [pageNumber, setPageNumber] = useState(1);
+// For Setting Geners List
   const [genreList, setGenreList] = useState([]);
+// For Setting Genres List that are checked 
   const [checkGenres, setCheckGenres] = useState([]);
+// For Setting Pages List 
+  const [pageNumber, setPageNumber] = useState(1);
+  //  Movies count getting from api calls
   const [movieCount,setMovieCount] = useState(0);
+  //  Total Movies count getting from api calls
   const [totalMovies,setTotalMovies] = useState(0);
+  //  filter using to get update from filter function to call api
   const [filter, setFilter] = useState(1);
+  // setting upadated from handleScroll function
   const [isBottom, setIsBottom] = useState(false);
+  // totalpage count getting from api calls
   const [totalPage,setTotalPage] = useState(0)
+  //  seacrh key word getting from page url
   const [searchTerm,setSearchTerm] = useState("")
+  // useNavigate hook to navigate the filter url
   const navigate = useNavigate();
+  // useLocation hook to get current page path
   const location = useLocation();
+  // useParams hook to get current page search term
   const key = useParams();
-
+// Initial useEffect to call api
   useEffect(() => {
     var searchParams = new URLSearchParams(location.search);
     var queryParams = {};
@@ -36,7 +48,7 @@ const MoviesList = () => {
         checkedList.map((val) => {
           if (!checkGenres.includes(val)) {
             checkGenres.push(val);
-            console.log(checkedList);
+            // console.log(checkedList);
           }
         });
       }
@@ -44,14 +56,18 @@ const MoviesList = () => {
     setSearchTerm(key.searchKey)
     searchKey(key.searchKey);
     Genre();
-    console.log("searchParams", searchParams);
+    // console.log("searchParams", searchParams);
     // FilteredMovies();
   }, []);
+  // UseEffect function to filter
   useEffect(() => {
     var searchParams = new URLSearchParams(location.search);
+    console.log("searchParams",searchParams)
     var queryParams = {};
     for (const param of searchParams.keys()) {
       const value = searchParams.get(param);
+      // console.log("value",value)
+      // console.log("value",decodeURIComponent(value))
       queryParams[param] = decodeURIComponent(value);
     }
     if (queryParams) {
@@ -60,7 +76,7 @@ const MoviesList = () => {
         checkedList.map((val) => {
           if (!checkGenres.includes(val)) {
             checkGenres.push(val);
-            console.log(checkedList);
+            // console.log(checkedList);
           }
         });
       }
@@ -69,7 +85,7 @@ const MoviesList = () => {
     searchKey(key.searchKey);
     // FilteredMovies();
   }, [filter]);
- 
+  // searchKey function to get data from api
   const searchKey = async (key) => {
     const { data } = await axios.get(
       checkGenres.length > 0
@@ -78,21 +94,21 @@ const MoviesList = () => {
           }`
         : `https://api.themoviedb.org/3/search/movie?api_key=1452e6e0980f76d9c09368379bd64adf&query=${key}&page=${pageNumber}`
     );
-    console.log("Search_List", data);
     setSearchList(data.results);
     setMovieCount(data.results.length)
     setTotalMovies(data?.total_results)
     setTotalPage(data.total_pages)
   };
+  // Api call to get Geners List
   const Genre = async (key) => {
     const { data } = await axios.get(
       `https://api.themoviedb.org/3/genre/movie/list?api_key=1452e6e0980f76d9c09368379bd64adf&language=en-US`
     );
-    console.log("Genre_List", data);
+    // console.log("Genre_List", data);
     setGenreList(data.genres);
    
   };
-  
+  // function to check if Gener is checked or not
   const GenresChecked = (text) => {
     var isChecked = false;
     if (checkGenres && checkGenres.length > 0) {
@@ -102,11 +118,11 @@ const MoviesList = () => {
     }
     return isChecked;
   };
-
+//Function to update Checked Gener List
   const getGenresFilter = async (e) => {
     console.log("GenreStatus", e.target);
     const { value, checked } = e.target;
-    console.log(value, checked);
+    console.log("checkedStatus",value, checked);
     if (checked) {
       if (checkGenres.length > 0) {
         checkGenres.pop();
@@ -115,9 +131,10 @@ const MoviesList = () => {
         checkGenres.push(value.toString());
       }
     }
-    console.log("updatedGenres", checkGenres);
+    // console.log("updatedGenres", checkGenres);
     genreStatusFilter();
   };
+  // function to update and navigate the checkGenres
   const genreStatusFilter = () => {
     var genresQuery = checkGenres.length > 0 ? checkGenres : "";
     const list = {};
@@ -128,8 +145,8 @@ const MoviesList = () => {
       pathname: location.pathname,
       search: `?${createSearchParams(list)}`,
     };
-    navigate(options, { replace: true });
-    console.log(list);
+    navigate(options, { replace: false });
+    // console.log(list);
     setFilter(filter + 1);
   };
  //function call for pagination  when scroll to bottom of page
@@ -140,7 +157,9 @@ const MoviesList = () => {
     const scrollHeight =
       document.documentElement.scrollHeight || document.body.scrollHeight;
     const windowHeight = window.innerHeight;
-
+    console.log("scrollHeight",document.documentElement.scrollHeight)
+    console.log("windowHeight",windowHeight)
+    console.log("scrollTop",document.documentElement.scrollTop)
     // Check if the user has scrolled to the bottom
     if (scrollTop + windowHeight >= scrollHeight - 10) {
       setIsBottom(true);
@@ -154,19 +173,21 @@ const MoviesList = () => {
   };
   // eslint-disable-next-line
 }, []);
+// function to update the page number if isBottom is true
 useEffect(() => {
   if (isBottom) {
-    
     setPageNumber(pageNumber + 1)
   }
   return;
   // eslint-disable-next-line
 }, [isBottom]);
+// function to handle page change
 useEffect(() => {
   if (pageNumber <= totalPage) {
     searchKey();
   }
 },[pageNumber])
+
   return (
     <div className="d-flex justify-content-between">
       <nav id="sidebarMenu " style={{ width: "20em" }}>
