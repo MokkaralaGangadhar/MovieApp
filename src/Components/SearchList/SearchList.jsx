a2ae2fd31eec9e99010180305bf716354c102bb0
MokkaralaGangadhar <114174897+MokkaralaGangadhar@users.noreply.github.com>
2023-11-29 09:14:06 -0800

    just newly added

--- src/Components/SearchList/SearchList.jsx
+++ src/Components/SearchList/SearchList.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 const SearchList = () => {
+    const [searchMovies,setSearchMovies] = useState("");
+    const [movieGenre,setMovieGenre] = useState("");
+    const [pageNumber,setPageNumber] = useState(1);
+
+    useEffect( () => {
+        searchKey();
+
+    },[])
+    const searchKey = async () => {
+        const { data } = await axios.get(
+
+         `https://api.themoviedb.org/3/discover/movie?api_key=1452e6e0980f76d9c09368379bd64adf&with_original_language=te&include_video=false&page=${pageNumber}`);
+         console.log(data);
+      }; 
+      
+    
+      // Api call to get Geners List
+      const Genre = async (key) => {
+        const { data } = await axios.get(
+          `https://api.themoviedb.org/3/genre/movie/list?api_key=1452e6e0980f76d9c09368379bd64adf&language=en-US`
+        );
+        // console.log("Genre_List", data);
+       
+      };
   return (
     <div>SearchList</div>
   )
